a7ca88ce5da5844c9a9776a927441312bed13513
KrysenkaAlina <sashaa2508@mail.ru>
2022-06-23 21:15:00 +0300

    commit lessons6

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
     public static void Main(String[] args) {
         int x = 1;
-         System.out.println(x);
+        String name = "Sashaa";
+         System.out.println(name);
     }
 }
